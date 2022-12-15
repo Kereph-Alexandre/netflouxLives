9ce09ef2c39a728ea1a09d038ec9d18119bced54
Alexandre Bouriez <alexandre.bouriez@laposte.net>
2022-12-15 14:50:16 +0100

    fix: Changement du chemin vers Logo

--- src/components/LogoNetFloux/LogoNetfloux.jsx
+++ src/components/LogoNetFloux/LogoNetfloux.jsx
@@ -3,7 +3,10 @@ import "./LogoNetfloux.css";
 export default function LogoNetfloux() {
   return (
     <>
-      <img src="/Logo_NetFloux_nobackground.png" alt="Logo du site NetFloux" />
+      <img
+        src="/assets/Logo_NetFloux_nobackground.png"
+        alt="Logo du site NetFloux"
+      />
     </>
   );
 }
