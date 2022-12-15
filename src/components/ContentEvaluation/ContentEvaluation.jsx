a279143f73e2d4765d527997a893475d99d3fd4b
Kereph-Alexandre <114568855+Kereph-Alexandre@users.noreply.github.com>
2022-12-15 20:57:47 +0100

    Docs: documentation composant Evaluation :memo:

--- src/components/ContentEvaluation/ContentEvaluation.jsx
+++ src/components/ContentEvaluation/ContentEvaluation.jsx
@@ -1,5 +1,10 @@
 import { Icon } from "@iconify/react";
 
+/**
+ * Affiche la note globale d'un média après évaluation des utilisateurs
+ * @param {note: number} props 
+ * @returns un élément contenant une note et une icône
+ */
 export default function ContentEvaluation(props) {
   return (
     <aside className="contentEvaluation">
