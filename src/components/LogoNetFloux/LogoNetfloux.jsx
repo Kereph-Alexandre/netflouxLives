ae8cfd8bf64ce45dcbfe2411b5dff940746daded
Alexandre Bouriez <alexandre.bouriez@laposte.net>
2022-12-15 14:36:49 +0100

    feat: Ajout du LogoNetfloux :sparkles:

---
+++ src/components/LogoNetFloux/LogoNetfloux.jsx
@@ -0,0 +1,9 @@
+import "./LogoNetfloux.css";
+
+export default function LogoNetfloux() {
+  return (
+    <>
+      <img src="/Logo_NetFloux_nobackground.png" alt="Logo du site NetFloux" />
+    </>
+  );
+}
