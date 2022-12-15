1983e478d69659ac84fff61952f93f1f9e5eb305
Kereph-Alexandre <114568855+Kereph-Alexandre@users.noreply.github.com>
2022-12-15 20:54:48 +0100

    Docs: documentation composant Panneau :memo:

--- src/components/DescribePanel/DescribePanel.jsx
+++ src/components/DescribePanel/DescribePanel.jsx
@@ -1,14 +1,24 @@
 import ContentCompatibility from "../ContentCompatibility/ContentCompatibility";
+import ContentEvaluation from "../ContentEvaluation/ContentEvaluation";
 import DescribeText from "../DescribeText/DescribeText";
 import DescribeTitle from "../DescribeTitle/DescribeTitle";
 
+/**
+ * Affiche les différentes information relatives à un média (film, série, chaine YT, musique, jeu)
+ * @param { titre:string,
+ * text:string,
+ * compatibility: number,
+ * note: number
+ * } props
+ * @returns Un panneau descriptif du média correspondant
+ */
 export default function DescribePanel(props) {
   return (
     <article className="describePanel">
       <DescribeTitle titre={props.titre} />
       <DescribeText text={props.text} />
       <ContentCompatibility compatibility={props.compatibility} />
-      {/* <ContentEvaluation note={props.note} /> */}
+      <ContentEvaluation note={props.note} />
     </article>
   );
 }
