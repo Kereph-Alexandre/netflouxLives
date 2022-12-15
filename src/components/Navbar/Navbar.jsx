7834c36a4afb7ada26079bfbdd76fc29e6320cee
Alexandre Bouriez <alexandre.bouriez@laposte.net>
2022-12-15 13:06:21 +0100

    Doc: documentation composant Navbar :memo:

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
 import NavbarItem from "../NavbarItem/NavbarItem";
 
+/**
+ * Retourne un composant NavbarItem pour chaque entrée de la liste passée en paramètre
+ * @param {item:string}
+ * @returns La liste de liens
+ */
 export default function Navbar({ items }) {
   console.log(items);
   return (
     <>
-      {items.map((item) => (
-        <NavbarItem text={item} />
-      ))}
+      <ul>
+        {items.map((item, index) => (
+          <NavbarItem text={item} key={index} />
+        ))}
+      </ul>
     </>
   );
 }
