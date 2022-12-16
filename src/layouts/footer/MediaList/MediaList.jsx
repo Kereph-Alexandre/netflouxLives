f8ed747b7ba8dfc1fc9d56469a91a08a739f60d2
Alexandre Bouriez <alexandre.bouriez@laposte.net>
2022-12-16 14:56:46 +0100

    feat: Début de liste de films, série, etc.

---
+++ src/layouts/footer/MediaList/MediaList.jsx
@@ -0,0 +1,27 @@
+import "./MediaList.css";
+import { Icon } from "@iconify/react";
+
+export default function MediaList(props) {
+  return (
+    <section>
+      <h2 className="listTitle">{props.titre}</h2>
+      <div className="listModule">
+        <div className="imageList">
+          <span>
+            <img src={props.src} alt="extrait de film" />
+          </span>
+          <span>
+            <img src={props.src} alt="extrait de film" />
+          </span>
+          <span>
+            <img src={props.src} alt="extrait de film" />
+          </span>
+          <span>
+            <img src={props.src} alt="extrait de film" />
+          </span>
+        </div>
+        <Icon icon="ic:baseline-keyboard-double-arrow-right" />
+      </div>
+    </section>
+  );
+}
